6d31e1d609397965637cd9b98b932f25e680de0a
lacomotiveman <lacomotiveman@inbox.ru>
2019-07-16 13:28:41 +0700

    Update todo-filter.js

--- src/components/todo-filter/todo-filter.js
+++ src/components/todo-filter/todo-filter.js
@@ -8,37 +8,40 @@ class TodoFilter extends Component{
 
 	render(){
 		return(
-			<div className="row">
-				<p> Priority Filter </p>
-				
-				<select id = {'PriFilter'} onChange = { this.onChange }>
-					
-					<option 
-						value={'all'} 
-						defaultValue={(this.props.selectedPriority === '')}
-						>All
-					</option>
+		<div className="row">
+			<div className="panel panel-default">
+				<div className="panel-heading">Priority Filter</div>
+				<div class="panel-body">
+					<select id = {'PriFilter'} onChange = { this.onChange }>
+						
+						<option 
+							value={'all'} 
+							defaultValue={(this.props.selectedPriority === '')}
+							>All
+						</option>
 
-					<option 
-						value={'high'} 
-						defaultValue={(this.props.selectedPriority === 'high')}
-						>High
-					</option>
+						<option 
+							value={'high'} 
+							defaultValue={(this.props.selectedPriority === 'high')}
+							>High
+						</option>
+						
+						<option 
+							value={'medium'}
+							defaultValue={(this.props.selectedPriority === 'medium')}
+							>Medium
+						</option>
+						
+						<option 
+							value={'normal'}
+							defaultValue={(this.props.selectedPriority === 'normal')}
+							>Normal
+						</option>
 					
-					<option 
-						value={'medium'}
-						defaultValue={(this.props.selectedPriority === 'medium')}
-						>Medium
-					</option>
-					
-					<option 
-						value={'normal'}
-						defaultValue={(this.props.selectedPriority === 'normal')}
-						>Normal
-					</option>
-				
-				</select>
+					</select>
+				</div>
 			</div>
+		</div>
 		);
 	}
 }
